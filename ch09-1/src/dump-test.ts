272a52f51628bb510ceb7c249719e5bee5a3453a
ssuh0o0 <ssuh0o0@daum.net>
2021-07-20 01:24:06 +0900

    ch09-1

---
+++ ch09-1/src/dump-test.ts
@@ -0,0 +1,5 @@
+import * as R from 'ramda'
+import {dump} from './dump'
+// import {dump} from './dump2'
+
+dump(R.range(1, 10))
---
+++ ch09-1/src/range.ts
@@ -0,0 +1,4 @@
+import * as R from 'ramda'
+console.log(
+    R.range(1, 9+1)
+)
